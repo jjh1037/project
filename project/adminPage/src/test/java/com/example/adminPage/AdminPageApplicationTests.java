e5b1903850870389ca69ae30616b16391ea71cbe
jjh1037 <thwibe1037@gmail.com>
2023-12-08 16:52:02 +0900

    Add files via upload

---
+++ project/adminPage/src/test/java/com/example/adminPage/AdminPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
